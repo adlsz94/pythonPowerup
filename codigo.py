275148103639fb92585065a004cd0ee6dc35475b
Adelson Developer <desouzaadelson@hotmail.com>
2024-03-04 21:32:43 -0300

    auto open edge browser

---
+++ codigo.py
@@ -0,0 +1,21 @@
+# arquivo codigo.py
+# Passo a Passo do Projeto
+# Passo 1: Entrar no sistema da empresa
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados
+# Passo 4: Cadastrar 1 produto
+# Repetir o processo de cadastro até acabar.
+
+# Instalar pyautogui
+##sudo apt install python3-pip
+## pipi install pyautogui
+
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> pressionar uma tecla do teclado
+
+import pyautogui
+
+pyautogui.press("win")
+pyautogui.write("edge")
+pyautogui.press("enter")
